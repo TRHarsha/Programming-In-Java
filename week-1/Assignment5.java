61c5f30f83173b49a0852843f9b2a7dad40af08f
Harsha T R <63777708+TRHarsha@users.noreply.github.com>
2023-02-07 17:12:24 +0530

    Create Assignment5.java

---
+++ week-1/Assignment5.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;  
+public class Exercise1_2 {
+       public static void main(String[] args) {
+Scanner s = new Scanner(System.in); 
+        int x = s.nextInt(); 
+        int y = s.nextInt();
+int z = s.nextInt();
+int result = 0;
+    if (x >= y && x >= z) {
+      System.out.println(x);
+    } else if (y >= x && y >= z) {
+      System.out.println(y);
+    } else {
+      System.out.println(z);
+    }
+       }
+}
